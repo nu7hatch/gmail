aad0891b7dbd357f725b29d4a19bb2c567739a89
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-10-21 14:40:09 -0700

    [spec-helper] cleanup account information into external account.yml move test helpers into spec-helper fill in mailbox_spec's last pending tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,26 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
 require 'rspec'
 require 'mocha'
+require 'yaml'
 require 'gmail'
 
 RSpec.configure do |config| 
   config.mock_with :mocha
 end
 
-TEST_ACCOUNT = ["test@gmail.com", "test"]
+def within_gmail(&block)
+  gmail = Gmail.connect!(*TEST_ACCOUNT)
+  yield(gmail)
+  gmail.logout if gmail
+end
+
+def mock_mailbox(box="INBOX", &block)
+  within_gmail do |gmail|
+    mailbox = subject.new(gmail, box)
+    yield(mailbox) if block_given?
+    mailbox
+  end
+end
+
+# Run test by creating your own test account with credentials in account.yml
+TEST_ACCOUNT = YAML.load_file(File.join(File.dirname(__FILE__), 'account.yml'))
