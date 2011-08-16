e200d23907640a78604ba8151e55ed6851003564
Aubrey Holland <aubreyholland@gmail.com>
2011-08-16 12:21:17 -0400

    Revert "sfsddf"
    This reverts commit e83dc177f160fe32815674e8b50afde541e36ba8.

--- lib/gmail/version.rb
+++ lib/gmail/version.rb
@@ -1,9 +1,13 @@
 module Gmail
   class Version #:nodoc:
-    STRING = '0.4.0.brewster'
+    MAJOR  = 0
+    MINOR  = 4
+    PATCH  = 0
+    EXTRA  = 'brewster'
+    STRING = [MAJOR, MINOR, PATCH, EXTRA].join('.')
   end # Version
 
   def self.version # :nodoc:
-    '0.4.0.brewster'
+    Version::STRING
   end
 end # Gmail
