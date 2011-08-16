6c3f7603866a130cd4354f0f6a18b9bf75afdfed
Aubrey Holland <aubreyholland@gmail.com>
2011-08-16 12:15:45 -0400

    add brewster suffix

--- lib/gmail/version.rb
+++ lib/gmail/version.rb
@@ -3,10 +3,11 @@ module Gmail
     MAJOR  = 0
     MINOR  = 4
     PATCH  = 0
-    STRING = [MAJOR, MINOR, PATCH].join('.')
+    EXTRA  = 'brewster'
+    STRING = [MAJOR, MINOR, PATCH, EXTRA].join('.')
   end # Version
-  
+
   def self.version # :nodoc:
     Version::STRING
-  end 
+  end
 end # Gmail
